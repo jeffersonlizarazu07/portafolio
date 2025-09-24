859f51bdbcc0d7b490feb8707cd66fed8730dcb6
Jefferson Lizarazu <Jeffersonlizarazu@hotmail.com>
2025-09-24 13:49:06 -0500

    Actualización

---
+++ mi-portafolio/src/navbar.jsx
@@ -0,0 +1,18 @@
+import { Box, List, ListItem } from "@mui/material";
+
+const Navbar = () => {
+    return (
+    
+    <Box>
+        <Box>
+
+        </Box>
+      <List>
+        <ListItem>Inicio</ListItem>
+        <ListItem>Sobre mí</ListItem>
+        <ListItem>Proyectos</ListItem>
+        <ListItem>Contacto</ListItem>
+      </List>
+    </Box>
+    );
+};
