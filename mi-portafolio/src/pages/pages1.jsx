c93fdd0fa82d61ef7d4fa4057f40cebd3d934963
Jefferson Lizarazu <Jeffersonlizarazu@hotmail.com>
2026-01-09 14:05:16 -0500

    Maincard created

--- mi-portafolio/src/pages/pages1.jsx
+++ mi-portafolio/src/pages/pages1.jsx
@@ -1,7 +1,29 @@
+import { Box, Typography } from "@mui/material";
 import { Navbar } from "../components/navbar";
+import { MainCard } from "../components/MainCard";
 
 export const FrontPage = () => {
     return (
-        <Navbar />
+        <Box>
+            <Navbar />
+            <Box>
+                <Typography>Sobre mi</Typography>
+                <Typography>
+                    Hola. Mi nombre es Jefferson Lizarazu. Soy desarrollador full stack y he decidido crear un lugar donde pueda compartir con ustedes mis proyectos realizados y aquellos proyectos que me encuentro realizando actualmente.
+                </Typography>
+            </Box>
+
+            <Box>
+                <MainCard/>
+                <Typography>
+                    Proyectos
+                </Typography>
+            </Box>
+
+
+        </Box>
+        
+
+        
     )
 }
